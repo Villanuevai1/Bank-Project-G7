a256a398b60a7223afba9563fdad2160dc957c6d
Briana Barajas <bribarajas24@gmail.com>
2024-04-22 10:47:46 -0700

    Removed duplicate files

--- index.php
+++ index.php
@@ -1,59 +1,45 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-
-    <title>Registration</title>
-
-    <style>
-        h1 {
-            color: darkgreen;
-        }
-
-        body {
-            background-color: lightgray;
-        }
-
-        input[type=button],
-        input[type=submit] {
-            background-color: darkblue;
-            border: none;
-            color: #fff;
-            padding: 15px 30px;
-            text-decoration: none;
-            margin: 4px 2px;
-            cursor: pointer;
-        }
-    </style>
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="src/index.css">
+    <title>Bank Login</title>
 </head>
-
 <body>
-
-    <h1><center>Registration</h1>
-
-    <form action="/process.php" method="post">
-
-
-
-        <h3>Username: <input type="text" name="username"></h3>
-
-        <h3>Password: <input type="password" name="password"></h3>
-
-        <h3>Address: <input type="text" name="address"></h3>
-
-        <h3>City: <input type="text" name="city"></h3>
-
-        <h3>Zip Code: <input type="text" name="zip"></h3>
-
-        <h3>Driver's License Number: <input type="text" name="DriverL"></h3>
-
-        <h3>Social Security Number (SSN): <input type="text" name="SSN"></h3>
-
-        <br>
-
-        <input type="submit" value="Register">
-    </form>
-
+    <section class="vh-100 gradient-custom">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card card-custom">
+                        <div class="card-body p-5 text-center">
+                            <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                            <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                                <div class="form-outline form-white">
+                                    <input type="text" id="username" name="username" class="form-control form-control-lg" required>
+                                    <label for="username" class="form-label">Username</label>
+                                </div>
+                                <div class="form-outline form-white">
+                                    <input type="password" id="password" name="password" class="form-control form-control-lg" required>
+                                    <label for="password" class="form-label">Password</label>
+                                </div>
+                                <button type="submit" class="btn btn-outline-light btn-lg px-5">Login</button>
+                            </form>
+                            <div class="mt-4">
+                                <a href="#" class="text-white"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#" class="text-white mx-4"><i class="fab fa-twitter"></i></a>
+                                <a href="#" class="text-white"><i class="fab fa-google"></i></a>
+                            </div>
+                            <p class="mb-0">Don't have an account? <a href="src/register.php" class="text-white-50 fw-bold">Sign Up</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
