7db550ca410c8999b293b04a30ea4263cabedfa8
Briana Barajas <95547837+brianabarajas@users.noreply.github.com>
2024-04-29 22:33:11 -0700

    Revised checking.php

---
+++ db/mirgations/01_checkingtable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS 'checkinginfo' (
+    'accountname' VARCHAR(255) NOT NULL,
+    'balance' INT NOT NULL,
+    'pin' VARCHAR(4) NOT NULL,
+    'credit_card_number' BIGINT NOT NULL,
+    'id' INT AUTO_INCREMENT PRIMARY KEY,
+    'account_number' BIGINT NOT NULL,
+    
+    'username' VARCHAR(255) NOT NULL,
+    FOREIGN KEY ('username') REFERENCES 'customers'('username')
+);
